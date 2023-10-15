911db4b6c7e0ed9b283ffc68f2dae4562eb6e76a
Lorenzo <beccarolorenzo@gmail.com>
2023-10-15 22:45:45 +0200

    - updated to express-beans v1.1.0

--- src/services/ExampleService.ts
+++ src/services/ExampleService.ts
@@ -1,10 +1,14 @@
-import { Bean } from 'express-beans';
+import { Bean, Logger, InjectLogger } from 'express-beans';
 
 @Bean
 export default class ExampleService {
   private msg: string;
 
+  @InjectLogger('example')
+  private logger!: Logger;
+
   constructor() {
+    this.logger.debug('initializing msg field');
     this.msg = 'hello world!';
   }
 
